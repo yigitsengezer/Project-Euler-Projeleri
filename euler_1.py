312d5a027f6f7b3d38fb0e86835c5c64490807f8
Yiğit Şengezer <43137514+ysengezer6@users.noreply.github.com>
2019-11-12 19:43:42 +0300

    Add files via upload

---
+++ euler_1.py
@@ -0,0 +1,8 @@
+# 1 den 1000 e kadar olan sayılardan 5 veya 3 ün katı olan sayıların toplamı
+toplam = 0
+
+for i in range(1000):
+    if i % 5 == 0 or i % 3 == 0:
+        toplam += i
+
+print(toplam)
